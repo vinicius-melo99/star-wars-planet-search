2f3b2ae535c25f50179aa32ca2a953fd6283b441
Vinícius Melo <viniciusmelo317@gmail.com>
2023-02-28 18:37:18 -0300

    requisito 5 ok

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,60 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
+import apiMock from './mocks/apiMock';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testes da página', () => {
+  beforeEach(() => {
+    
+  })
+
+  it('verifica se a página carrega com todos os elementos', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(apiMock),
+    });
+    render(<App />);
+    const image = await screen.findByRole('img');
+    const nameFilterInput = await screen.findByTestId('name-filter');
+    const tableElement = await screen.findAllByTestId('planet-name');
+
+    expect(image).toBeInTheDocument();
+    expect(nameFilterInput).toBeInTheDocument();
+    expect(tableElement).toHaveLength(10);
+  });
 });
+
+describe('Testes de filtro', () => {
+  it('verifica se é possível filtrar os planetas pelo nome', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(apiMock),
+    });
+    render(<App />);
+    const nameFilterInput = await screen.findByTestId('name-filter');
+    userEvent.type(nameFilterInput, 'a');
+    const planets = await screen.findAllByTestId('planet-name');
+    expect(planets).toHaveLength(7);
+  
+  });
+  
+  it('verifica se os filtros aplicados são exibidos', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(apiMock),
+    });
+    render(<App />);
+    userEvent.selectOptions(
+      await screen.findByTestId('column-filter'),
+      'orbital_period',
+    );
+    const nameFilterInput = await screen.findByTestId('value-filter');
+    const buttonFilter = await screen.findByTestId('button-filter');
+    userEvent.type(nameFilterInput, '500');
+    userEvent.click(buttonFilter);
+    const filter = await screen.findByText(/orbital_period 0500/i);
+
+    expect(filter).toBeInTheDocument();
+  });
+})
